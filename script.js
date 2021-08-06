a3b1271134c8c01c3e027fbf20a83ef44cec7622
gjspencer22 <gjspencer22@gmail.com>
2021-08-06 02:12:18 -0600

    issue with always being correct

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const questionContainerElement = document.getElementById
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById
     ('answer-buttons')
+const tester1 = document.getElementById('oy')
 
 let shuffledQuestions, currentQuestionIndex
 var quizRunning = false;
@@ -68,8 +69,8 @@ function resetState() {
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild
             (answerButtonsElement.firstChild)
-            document.getElementById("answer").textContent = ""
-            
+        document.getElementById("answer").textContent = ""
+
     }
 }
 function selectAnswer(e) {
@@ -89,7 +90,7 @@ function selectAnswer(e) {
 }
 
 function correctAnswer() {
-   
+
     document.getElementById("answer").textContent = "Correct!"
 }
 function incorrect() {
@@ -100,19 +101,38 @@ function incorrect() {
 
 function setStatusClass(element, correct) {
     clearStatusClass(element)
-    if (correct) {
-        element.classList.add('correct'),
-            correctAnswer();
+    if (correct=true) {
+        element.classList.add('correct')
+        correctAnswer()
+        // blank()
     }
-    else {
+
+    if (correct=false) {
         element.classList.add('wrong')
-        incorrect();
+        incorrect()
+
     }
     function clearStatusClass(element) {
         element.classList.remove('correct')
         element.classList.remove('wrong')
     }
 
+}
+// function blank(element) {
+// document.getElementById("oy").textContent = ""
+// document.getElementById("oy").textContent = "oy"
+// correctAnswer();
+// }
+// function blank(element) {
+// document.getElementById("oy").textContent = ""
+// document.getElementById("oy").textContent = "oy"
+// correctAnswer();
+// }
+function tester(element, correct) {
+   
+    if (correct) {
+        // correctAnswer();
+    }
 }
 const questions = [
     {
@@ -124,28 +144,28 @@ const questions = [
             { text: 'All of the Above', correct: true }
 
         ]
-    }
-    // {
-    //     question: 'Inside which HTML element do we put the JavaScript?',
-    //     answers: [
+    },
+    {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        answers: [
 
-    //         { text: '<scripting>', correct: false },
-    //         { text: '<script>', correct: true },
-    //         { text: '<style>', correct: false },
-    //         { text: '<muffins>', correct: false }
+            { text: '<scripting>', correct: false },
+            { text: '<style>', correct: false },
+            { text: '<muffins>', correct: false },
+            { text: '<script>', correct: true }
 
-    //     ]
-    // },
+        ]
+    },
 
-    // {
-    //     question: 'How do you create a function in JavaScript?',
-    //     answers: [
-    //         { text: 'function myFunction()', correct: true },
-    //         { text: 'function = myFunction()', correct: false },
-    //         { text: 'function: myFunction()', correct: false },
-    //         { text: 'function> myFunction()', correct: false }
-    //     ]
-    // },
+    {
+        question: 'How do you create a function in JavaScript?',
+        answers: [
+            { text: 'function = myFunction()', correct: false },
+            { text: 'function: myFunction()', correct: false },
+            { text: 'function> myFunction()', correct: false },
+            { text: 'function myFunction()', correct: true }
+        ]
+    },
 
 
 ]
